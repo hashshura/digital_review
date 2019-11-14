cbd4ca4754d1c8c93048e3b95eef4306d4d01634
Asif Hummam Rais <hashshura@gmail.com>
2019-11-14 11:57:37 +0700

    Initial commit

---
+++ controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from odoo import http
+
+# class DigitalReview(http.Controller):
+#     @http.route('/digital_review/digital_review/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/digital_review/digital_review/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('digital_review.listing', {
+#             'root': '/digital_review/digital_review',
+#             'objects': http.request.env['digital_review.digital_review'].search([]),
+#         })
+
+#     @http.route('/digital_review/digital_review/objects/<model("digital_review.digital_review"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('digital_review.object', {
+#             'object': obj
+#         })
